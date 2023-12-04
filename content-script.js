a6cabf7ef838cce97b3ff7a771f7c90a60e11ee8
Stanley Zhao <stanleyhzhao@gmail.com>
2023-12-04 01:24:34 -0500

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -14,7 +14,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         document
           .querySelector("[data-track-load='description_content']")
           .innerText.replaceAll("\n\n", "\n")
-          .split("\n\n \n\nFollow up")[0]
+          .split("\n \nFollow up")[0]
       }\n"""\n\n${document
         .getElementsByClassName("view-lines")[0]
         .innerText.replace(/[^\x00-\x7F]/g, " ")}`,
